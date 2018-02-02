fb592d6ff6f9bb724ee032c18642c1a168a81391
Eric Freudenthal <eric@freudenthal.net>
2018-02-02 03:41:32 -0700

    added unsigned number generator

---
+++ unsigned-numbers/unums.py
@@ -0,0 +1,21 @@
+
+import random
+
+debug = False 
+
+def makeu():
+    numbits = random.randrange(3, 12)
+    zlsb = random.randrange(0, numbits)
+    zmsb = random.randrange(zlsb, numbits)
+    n = (1 << numbits) - 1
+    n ^= (1 << zmsb) - 1
+    n ^= (1 << zlsb) - 1
+    if debug: 
+        print "%s, %#x, %#o, %s" % (format(n, "#b"), n, n, (numbits, zmsb, zlsb))
+    return n
+
+
+for i in range(20):
+    formatChar = "xod"[i%3]
+    formatStr = "%%#%c" % formatChar
+    print formatStr % makeu()
